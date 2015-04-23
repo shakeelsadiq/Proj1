aa1c56c190f8a3aab134c3bd44a244dac21cac69
shak42@hotmail.com <shak42@hotmail.com>
2015-04-23 21:00:30 +0100

    played with routing

--- Proj1/App_Start/RouteConfig.cs
+++ Proj1/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace Proj1
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Products",
+                url: "matings",
+                defaults: new { controller = "Product", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            
         }
     }
 }
